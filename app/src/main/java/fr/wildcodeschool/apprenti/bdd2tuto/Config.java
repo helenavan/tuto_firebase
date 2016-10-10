d4a8266e544687afef02eead47c2472c8d9bff40
helenavan <tedone.bd@gmail.com>
2016-10-10 14:09:43 +0200

    tuto firebase

---
+++ app/src/main/java/fr/wildcodeschool/apprenti/bdd2tuto/Config.java
@@ -0,0 +1,9 @@
+package fr.wildcodeschool.apprenti.bdd2tuto;
+
+/**
+ * Created by apprenti on 03/10/16.
+ */
+
+public class Config {
+    public static final String FIREBASE_URL = "https://bdd2tuto.firebaseio.com/";
+}
---
+++ app/src/main/java/fr/wildcodeschool/apprenti/bdd2tuto/Person.java
@@ -0,0 +1,31 @@
+package fr.wildcodeschool.apprenti.bdd2tuto;
+
+/**
+ * Created by apprenti on 03/10/16.
+ */
+
+public class Person {
+    //name and address string
+    private String name;
+    private String address;
+
+    public Person() {
+      /*Blank default constructor essential for Firebase*/
+    }
+    //Getters and setters
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+}
